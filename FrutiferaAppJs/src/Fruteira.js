a300031fa27595414eed69a7aedd2ba4a8bedf35
Luiz Evangelista <luiz.evangelista@academico.ifpb.edu.br>
2026-02-26 10:42:16 -0300

    Update Fruteira.js

--- FrutiferaAppJs/src/Fruteira.js
+++ FrutiferaAppJs/src/Fruteira.js
@@ -1,6 +1,9 @@
+// Exporta a classe Fruteira para poder ser usada em outros arquivos
 export default class Fruteira {
 
+  //O construtor é usado para criar uma nova fruta
   constructor(id, nomePopular, nomeCientifico, producaoMedia, dataPlantio) {
+    // Os atribudos da fruta
     this.id = id;
     this.nomePopular = nomePopular;
     this.nomeCientifico = nomeCientifico;
@@ -8,16 +11,19 @@ export default class Fruteira {
     this.dataPlantio = dataPlantio;
   }
 
-  // Calcula idade em meses
+  // Esse é o metodo que calcula idade em meses
   calcularIdadeMeses() {
-    const partes = this.dataPlantio.split("/");
+    const partes = this.dataPlantio.split("/");// Separa dia,mês e ano com (/)
+    //Cria uma data no formato Dia,Mês e Ano
     const data = new Date(partes[2], partes[1] - 1, partes[0]);
-    const hoje = new Date();
-
+    const hoje = new Date();//Pega a data atual
+    
+    //Calcula a diferença de anos em meses
     let meses = (hoje.getFullYear() - data.getFullYear()) * 12;
-    meses += hoje.getMonth() - data.getMonth();
+    meses += hoje.getMonth() - data.getMonth();//soma a diferença de meses
 
-    return meses;
+    return meses;// Retorna a data em meses
   }
+
 
 }
