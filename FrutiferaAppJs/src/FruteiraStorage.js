e140a1a178a745bd4ec15b3f0211989c2bd5f8f7
Luiz Evangelista <luiz.evangelista@academico.ifpb.edu.br>
2026-02-26 10:24:51 -0300

    Refactor comments in FruteiraStorage.js
    Updated comments for clarity and added explanations for methods.

--- FrutiferaAppJs/src/FruteiraStorage.js
+++ FrutiferaAppJs/src/FruteiraStorage.js
@@ -1,22 +1,22 @@
+// Importa a classe Fruteira.js
 import Fruteira from './Fruteira.js';
 
+//Essa classe aqui vai ser a responsavel por salvar e buscar dados no navegador
 export default class FruteiraStorage {
 
-  // Busca todas as frutas salvas
+  // Busca todas as frutas salvas aqui no localStorage
   static buscar() {
     return JSON.parse(localStorage.getItem("fruteiras")) || [];
+    //Vai pegar os dados salvos e converter de texto pra objeto, se não tiver nada salvo vai retornar vazio
+    
   }
 
-  // Salva nova fruta
+  // Salva nova fruta aqui no localStorage
   static salvar(fruteira) {
-    const lista = this.buscar();
-    lista.push(fruteira);
+    const lista = this.buscar();//Busca a lista atual de frutas
+    lista.push(fruteira);//Adiciona a nova fruta na lista
     localStorage.setItem("fruteiras", JSON.stringify(lista));
+    //Salva a lista atualizada aqui localStorage e tambem converte o array pra texto
   }
-  // Apaga o card 
-  static remover(id) {
-    const lista = this.buscar();
-    const novaLista = lista.filter(f => f.id !== id);
-    localStorage.setItem("fruteiras", JSON.stringify(novaLista));
-  }
+
 }
